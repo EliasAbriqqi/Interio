1241796c56ee105b6ae547f52213ae5c73c26bfd
EliasAbriqqi <elias.abriqqi@gmail.com>
2022-10-21 15:03:37 +0300

    Api changes

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:3000/products";
+export const API_URL = "https://interio-api-server.herokuapp.com/products";
 export const TIMEOUT_SEC = 10;
 export const PIC_HOST = "https://castellano.ru/descargas/interio-pics/";
