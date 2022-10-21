3c2a86abe3785b13b4fd6bd012120da33d54a7e7
EliasAbriqqi <elias.abriqqi@gmail.com>
2022-10-21 14:26:44 +0300

    corrections

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import { create, router as _router, defaults } from "json-server";
-const server = create();
-const router = _router("./database/products.json");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./database/products.json");
 
-const middlewares = defaults();
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
