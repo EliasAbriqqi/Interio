2b2f9d9d7d156384caa6b0d5fdc8d396ff3115b2
EliasAbriqqi <elias.abriqqi@gmail.com>
2022-10-21 14:31:37 +0300

    one more intent

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("database/products.json");
+const router = jsonServer.router("./database/products.json");
 
 const middlewares = jsonServer.defaults();
 
