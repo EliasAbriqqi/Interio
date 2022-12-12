e59fec03214b2e6dc63b62cefe6edb2dbec7359b
eliasabriqqi <elias.abriqqi@gmail.com>
2022-12-12 16:21:32 +0300

    Api server changed to render.com

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://interio-api-server.herokuapp.com/products";
+export const API_URL = "https://interio-api-server.onrender.com/products";
 export const TIMEOUT_SEC = 10;
 export const PIC_HOST = "https://castellano.ru/descargas/interio-pics/";
